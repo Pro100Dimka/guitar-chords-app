00b78fc2b5e6e1e6f9eb515585ab2e4aa55d0b9c
Dmitriy Andreev <pro100dimka.and@gmail.com>
2026-03-17 23:39:48 +0200

    refractoring ios and add img

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { ImageBackground, Platform, StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { SystemBars } from "react-native-edge-to-edge";
@@ -8,10 +8,10 @@ import backgroundImage from "../assets/images/screen-background/background.jpg";
 import palette from "./theme/palette";
 import { initLanguage } from "./locales";
 import { initDatabase } from "./components/database";
+import SplashScreen from "react-native-splash-screen";
 
 const App: FC = () => {
   const [ready, setReady] = useState(false);
-
   useEffect(() => {
     const init = async () => {
       await initDatabase(); // дождались создания таблиц
@@ -20,10 +20,9 @@ const App: FC = () => {
     };
     init();
   }, []);
-
-  if (!ready) {
-    return <View style={styles.flex} />; // пустой экран/спиннер пока база не готова
-  }
+  useEffect(() => {
+    if (!ready && Platform.OS === "android") SplashScreen.hide();
+  }, [ready]);
 
   return (
     <SafeAreaProvider>
